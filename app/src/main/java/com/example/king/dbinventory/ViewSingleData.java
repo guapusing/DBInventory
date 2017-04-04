4b7b6607a290ebb1e069b72d95ca7372d3bd40c8
guapusing <rendisukirman.rs@gmail.com>
2017-04-04 20:22:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/king/dbinventory/ViewSingleData.java
@@ -0,0 +1,38 @@
+package com.example.king.dbinventory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.king.dbinventory.R;
+/**
+ * Created by king on 4/4/2017.
+ */
+
+public class ViewSingleData extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_single_data);
+
+        TextView tvNama = (TextView) findViewById(R.id.tv_nama_barang);
+        TextView tvMerk = (TextView) findViewById(R.id.tv_merk_barang);
+        TextView tvHarga = (TextView) findViewById(R.id.tv_harga_barang);
+
+        System.out.println("APPINVENT "+getIntent().getExtras().getString("nama"));
+        tvNama.setText("Barang "+getIntent().getExtras().getString("nama"));
+        tvMerk.setText("Merk "+getIntent().getExtras().getString("merk"));
+        tvHarga.setText("Harga "+getIntent().getExtras().getString("harga"));
+
+        Button buttonOK = (Button) findViewById(R.id.bt_ok);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
